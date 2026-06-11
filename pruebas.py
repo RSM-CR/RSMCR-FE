15c35b990c978aee1ddcdff413bba7e812308a59
Matheus Blanco Malcher <mblancom@rsm.cr>
2026-06-11 15:32:12 -0600

    intento

---
+++ pruebas.py
@@ -0,0 +1,21 @@
+#Encabezado de la factura
+
+Nombre= "RSM COSTA RICA AUDIT TAX AND CONSULTING SERVICES SOCIEDAD ANONIMA"
+CedJur="3101177137"
+CorrRec="facturasproveedorCEADR@iqvia.com"
+TelEm="41151500"
+CodigoPais="506"
+Provincia="1"
+Canton="2"
+Distrito="3"
+OtrasSenas="Edificio Las Terrazas Business Center 5to piso"
+
+
+#Detalles de la factura
+
+Codigo="002"
+Cantidad="1"
+PrecioUnitario="75.00"
+Detalle="Licencia uso de Sistema Xero de junio"
+Impuesto="13.00"
+

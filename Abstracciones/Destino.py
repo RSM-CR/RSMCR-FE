3773210a6db4feec7c579bb7a2a42e9cef45cda9
SPina-RSM <spina@rsm.cr>
2026-06-11 16:45:31 -0600

    Documentar abstracciones

--- Abstracciones/Destino.py
+++ Abstracciones/Destino.py
@@ -1,14 +1,20 @@
 from abc import ABC, abstractmethod
 
 class Destino(ABC):
+    """Representa el receptor de los datos generados por la [fuente](fuente.Fuente). Estos datos serán subidos al destino para que se sincronicen."""
     def __init__(self) -> None:
         pass
 
     # Falta el tipo de dato de factura
     @abstractmethod
     def subir_factura(self, factura):
+        """Sube la factura a la plataforma relevante. En caso de GTI, primero se hará la conversión a XML.
+        
+        > [!CAUTION]
+        > En caso de que ocurra cualquier error, se va a asumir que la fuente de verdad se encuentra en la [fuente](fuente.Fuente) que haya sido seleccionada. Esto puede causar que se sobreescriban datos en el destino para corregirlos."""
         pass
 
     @abstractmethod
     def obtener_documento(self):
+        """Obtiene un documento para verificar el resultado de la factura subida. En caso de la conexión de GTI a Xero, esto se va a usar para subir un PDF como archivo adjunto."""
         pass
--- Abstracciones/Fuente.py
+++ Abstracciones/Fuente.py
@@ -7,19 +7,29 @@ class CallbackNuevaFactura(Protocol):
         ...
 
 class Fuente(ABC):
-    def __init__(self):
+    """Representa una fuente de datos que se usará para recibir las facturas. Un ejemplo de una fuente de datos es GTI."""
+    def __init__(self) -> None:
         self.callbacks = []
 
     # Falta el tipo de dato del documento
     @abstractmethod
     def subir_documento(self, documento):
+        """Se usa para devolver un documento a la plataforma usada como fuente.
+        En el caso de Xero, esto se utiliza para devolver un PDF como archivo adjunto."""
         pass
 
     @abstractmethod
-    def escuchar_nueva_factura(self, funcion: CallbackNuevaFactura):
+    def escuchar_nueva_factura(self, funcion: CallbackNuevaFactura) -> None:
+        """Añade un callback que se ejecuta cada vez que se detecta una nueva factura.
+        El formato de este callback es dictado por [CallbackNuevaFactura](fuente.CallbackNuevaFactura).
+        
+        :param CallbackNuevaFactura funcion: La función o método que se ejecutará cuando se detecta la creación de una nueva factura"""
         self.callback = funcion
 
     # Falta añadir el tipo de dato de factura pero aún no está
     def notificar_nueva_factura(self, factura):
+        """Se encarga de llamar a todos los callbacks, pasándoles la factura que fue recibida.
+        
+        :param Factura factura: La factura que será enviada a los callbacks."""
         for callback in self.callbacks:
             callback(factura)

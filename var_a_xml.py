374cfbda6c60a28dd45870050bb62a1aaea7979a
Alexa Villalobos Araya <avillalobos@rsm.cr>
2026-06-16 14:37:54 -0600

    comentarios

--- var_a_xml.py
+++ var_a_xml.py
@@ -4,20 +4,20 @@ from factura import DatosXero
 #cargar los datos
 lista_facturas = DatosXero.obtener_datos("prueba.json")
 
-if len(lista_facturas) > 0:
+if len(lista_facturas) > 0: #esto hace que el bloque de código se ejecute si hay cierta cantidad de elementos específica
 
-    variables = lista_facturas[0] 
+    variables = lista_facturas[0]  #hace que en la lista se cuente desde el primer objeto, el 0
     
-    python = ["tipo", "cliente", "estado", "moneda", "lineas"]
+    python = ["tipo", "cliente", "estado", "moneda", "lineas"] #la lista de lo que se extrae de las variables de Datos Xero
 
-    root = ET.Element("traducido")
-    for varpy in python:
-        valor = getattr(variables, varpy, None)
+    root = ET.Element("traducido") #le da el nombre a la sección donde van a estar los datos
+    for varpy in python: #el for hace que se repita paso por paso hasta tener todos los objetos requeridos
+        valor = getattr(variables, varpy, None) #se asegura de obtener todo
 
-        if valor is not None:
-            sub_elemento = ET.SubElement(root, varpy)
-            sub_elemento.text = str(valor)
+        if valor is not None: #verifica que se encuentre la información
+            sub_elemento = ET.SubElement(root, varpy) #se crea la etiqueta del xml
+            sub_elemento.text = str(valor) #mete la información y convierte la variable en un texto plano que es lo que xml puede leer
 
-    tree = ET.ElementTree(root)
-    ET.indent(tree, space="   ")
-    tree.write("factura.xml", encoding="utf-8", xml_declaration=True)
+    tree = ET.ElementTree(root) #toma la estructura de etiquetas creada y las guarda para guardarlas en un archivo real.
+    ET.indent(tree, space="   ") #le da el formato estético al XML aplicando los saltos de línea y 3 esácios de sangría a cada etiqueta
+    tree.write("factura.xml", encoding="utf-8", xml_declaration=True) #crea y escribe el archivo xml definitivo

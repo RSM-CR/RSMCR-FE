4e80c2b7f7c92c620c10dcd582a44aaf91880dec
SPina-RSM <spina@rsm.cr>
2026-06-19 11:15:59 -0600

    Añadir cosas de la interfaz

---
+++ interfaz/src/lib/components/NavBar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { Tab } from "../tab";
+
+	interface Props {
+		tabs?: Tab[];
+	}
+
+    let { tabs = [] }: Props = $props();
+</script>
+
+<nav>
+    {#each tabs as tab}
+        <ul>
+            <a href={tab.ruta}>{tab.nombre}</a>
+        </ul>
+    {/each}
+</nav>
+
+<style>
+    nav
+    {
+        display: flex;
+        gap: 16px;
+        padding: 8px;
+    }
+</style>
---
+++ interfaz/src/lib/tab.ts
@@ -0,0 +1,11 @@
+export class Tab
+{
+    nombre: string;
+    ruta: string;
+
+    constructor(nombre: string, ruta: string)
+    {
+        this.nombre = nombre;
+        this.ruta = ruta;
+    }
+}

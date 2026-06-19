dfb19dbc9629d40650ee3ab336d4001457fc8d51
Julian Sánchez Vargas <jsanchezv@rsm.cr>
2026-06-19 15:34:50 -0600

    adding more quality control for the websockets

--- interfaz/src/routes/+layout.svelte
+++ interfaz/src/routes/+layout.svelte
@@ -2,6 +2,12 @@
     import NavBar from "$lib/components/NavBar.svelte";
     import { Tab } from "$lib/tab";
     import { resolve } from "$app/paths";
+    import { onMount } from "svelte";
+    import { connectWebSocket } from "$lib/websockets.service";
+
+    onMount(() => {
+        connectWebSocket()
+    })
 
     const urlActual = resolve("/editor");
 

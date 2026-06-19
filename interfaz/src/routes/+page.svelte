1abec075174f362def99c7e095e84e525d6611f8
Math3wsB <mblancom@rsm.cr>
2026-06-19 14:05:16 -0600

    lista RFiles

--- interfaz/src/routes/+page.svelte
+++ interfaz/src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>Mira <a href="./ejemplo">mi pagina</a></p>
+<p>Mira <a href="./RFiles">mi pagina</a></p>

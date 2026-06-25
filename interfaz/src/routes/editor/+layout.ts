96bfd9fd8feb84560eb85147e861cde0ea036910
SPina-RSM <spina@rsm.cr>
2026-06-25 09:19:40 -0600

    Quitar import

--- interfaz/src/routes/editor/+layout.ts
+++ interfaz/src/routes/editor/+layout.ts
@@ -1,5 +1,3 @@
-import { Factura } from "$lib/factura";
-
 // Aquí se obtendría y se cargaría la factura en el futuro desde la API
 export async function load({ params, fetch }) {
     // const factura = new Factura();
